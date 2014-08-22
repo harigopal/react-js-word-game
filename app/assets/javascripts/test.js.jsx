a5b1156258f0b864639c296db2fdd2fb70ce7c2b
Hari Gopal <mail@harigopal.in>
2014-08-22 15:58:59 +0530

    Adding one more React class and building a little game-like thingy.

--- app/assets/javascripts/test.js.jsx
+++ app/assets/javascripts/test.js.jsx
@@ -1,31 +1,54 @@
 /** @jsx React.DOM */
 
-//var mountNode = document.getElementById('mount-node');
-
-var HelloMessage = React.createClass({
-  render: function() {
-    return <div>Hello {this.props.name}</div>;
+var outputStyle = function (successState) {
+  if (successState) {
+    return {
+      backgroundColor: '#66FF66'
+    }
+  } else {
+    return {
+      backgroundColor: 'default'
+    }
   }
-});
+};
+
+var resultRenderer = function () {
+  return (
+    <div>
+      <input
+      ref='input'
+      onChange={this.handleChange} />
+      <p style={outputStyle(this.state.succeeded)}>{this.state.value}</p>
+    </div>
+    )
+};
 
 var Duplicator = React.createClass({
-  getInitialState: function() {
-    return {value: 'Nothing has been typed yet.'};
+  getInitialState: function () {
+    return { value: 'Nothing has been typed yet.', succeeded: false };
   },
-  handleChange: function() {
-    this.setState({value: this.refs.input.getDOMNode().value});
+  handleChange: function () {
+    var newValue = this.refs.input.getDOMNode().value
+    this.setState({value: newValue});
+
+    if (newValue === 'Hello world!') {
+      this.setState({succeeded: true});
+    }
   },
-  render: function() {
-    return (
-      <div>
-        <input
-          ref='input'
-          onChange={this.handleChange} />
-        <h3>Duplicate</h3>
-        <p>{this.state.value}</p>
-      </div>
-    )
-  }
+  render: resultRenderer
 });
 
-//React.renderComponent(<HelloMessage name="John" />, mountNode);
+var Reverser = React.createClass({
+  getInitialState: function () {
+    return { value: 'Start typing!', succeeded: false };
+  },
+  handleChange: function () {
+    var newValue = this.refs.input.getDOMNode().value.split("").reverse().join("");
+    this.setState({value: newValue});
+
+    if (newValue === 'I think, therefore I am.') {
+      this.setState({succeeded: true});
+    }
+  },
+  render: resultRenderer
+});
